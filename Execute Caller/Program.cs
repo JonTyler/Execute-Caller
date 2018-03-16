cdbbc2f0bbdeac1e0e2d76db6e3bcef515b9d5c2
Jonathon Tyler <jonbtyler@gmail.com>
2018-03-15 23:59:19 -0400

    Add project files.

---
+++ Execute Caller/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace Execute_Caller
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string targetFile = "";
+            Process executibleFile = new Process();
+            targetFile = ConfigurationManager.AppSettings["Name"];
+            try
+            {
+                executibleFile.StartInfo.UseShellExecute = true;
+                executibleFile.StartInfo.FileName = targetFile;
+                executibleFile.Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
